424d2e71b82eb79dea0ae959dc156b9b55985488
andreburg <andreludovicusburger@gmail.com>
2024-09-28 15:26:26 +0200

    feat(auth): Add login page
    Added login page with the same layout as the registration page.
    
    refs: #87

--- web-app/src/app/auth/login/page.tsx
+++ web-app/src/app/auth/login/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema } from "@/lib/schemas";
 import {
   FormField,
@@ -9,8 +7,22 @@ import {
   FormLabel,
   FormControl,
   FormMessage,
+  Form
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { logIn } from "@/components/actions/authActions";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import Link from "next/link";
 
 type LoginFormInputs = {
   email: string;
@@ -19,46 +31,74 @@ type LoginFormInputs = {
 
 export default function Login() {
   const form = useForm<LoginFormInputs>({
-    resolver: zodResolver(loginSchema),
+    resolver: zodResolver(loginSchema)
   });
 
-  const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
-    console.log(data);
+  const onSubmit = async (data: FormData) => {
+    await logIn(data);
   };
 
   return (
-    <FormProvider {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Your Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className="w-full h-full flex flex-col items-center p-14 lg:px-20 max-sm:px-5 gap-20 max-md:gap-10">
+      <h1 className="col-span-2 text-4xl text-brand-tertiary">Welcome Back</h1>
+      <Card className="w-full">
+        <Form {...form}>
+          <form action={onSubmit} className="w-full">
+            <CardHeader>
+              <CardTitle className="text-2xl text-muted-foreground">
+                Log In
+              </CardTitle>
+              <CardDescription>
+                Welcome back please enter your details.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-5 grid-cols-1">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Your Password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <button type="submit">Login</button>
-      </form>
-    </FormProvider>
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col gap-2">
+                <div>
+                  <Button type="submit">Register</Button>
+                </div>
+                <div className="flex gap-1">
+                  <p>Don't have account?</p>
+                  <Link href="/auth/register" className="text-brand-tertiary">
+                    Register
+                  </Link>
+                </div>
+              </div>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </div>
   );
 }
