8be6958bc4e25ae330e5c0a6bf8d36ed719da4ca
andreburg <andreludovicusburger@gmail.com>
2024-09-28 13:12:56 +0200

    fix(database): Fix prisma export using singleton object
    Fix prisma export only exporting singleton object as exporting prisma creates too many instances crashing the app.
    
    refs: #91

--- web-app/src/lib/db/index.ts
+++ web-app/src/lib/db/index.ts
@@ -1,2 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-export const db = new PrismaClient();
+
+export * from "@/lib/db/comment";
+export * from "@/lib/db/device";
+export * from "@/lib/db/follow";
+export * from "@/lib/db/post";
+export * from "@/lib/db/prediction";
+export * from "@/lib/db/system-history";
+export * from "@/lib/db/system";
+export * from "@/lib/db/user-history";
+export * from "@/lib/db/user";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
