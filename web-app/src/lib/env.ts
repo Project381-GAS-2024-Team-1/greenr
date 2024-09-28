4f20bb59fff19174d1042231a58caaa15323f4a6
andreburg <andreludovicusburger@gmail.com>
2024-09-28 15:15:47 +0200

    build(env-vars): Add env.ts for centralized access to env vars
    add env.ts to make sure that all environment variables has a value. This will make it possible to warn us when it is not set, but not crash our development environments when doing so.

--- web-app/src/lib/env.ts
+++ web-app/src/lib/env.ts
@@ -1,1 +1,17 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "sdfs";
+export const JWT_SECRET =
+  process.env.JWT_SECRET ||
+  (() => {
+    console.warn(
+      "environment variable `JWT_SECRET` is not set.\nreverting to default."
+    );
+    return "dev_secret";
+  })();
+
+export const HASH_KEY =
+  process.env.HASH_KEY ||
+  (() => {
+    console.warn(
+      "environment variable `HASH_KEY` is not set.\nreverting to default."
+    );
+    return "dev_hash";
+  })();
